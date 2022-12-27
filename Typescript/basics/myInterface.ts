e9b3ce4e6c210aa0f892ef280b95724657480302
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2022-12-27 21:16:30 +0530

    Completed pure ts

--- Typescript/basics/myInterface.ts
+++ Typescript/basics/myInterface.ts
@@ -1,30 +1,36 @@
 interface User {
-    readonly dbId: number
-    email: string,
-    userId: number,
-    googleId?: string
-    // startTrail: () => string
-    startTrail(): string
-    getCoupon(couponname: string, value: number): number
+  readonly dbId: number;
+  email: string;
+  userId: number;
+  googleId?: string;
+  // startTrail: () => string
+  startTrail(): string;
+  getCoupon(couponname: string, value: number): number;
 }
 
+// re-opening of interface
 interface User {
-    githubToken: string
+  githubToken: string;
 }
+
+// A type cannot be changed after creating
 
 interface Admin extends User {
-    role: "admin" | "ta" | "learner"
+  role: "admin" | "ta" | "learner";
 }
 
-const hitesh: Admin = { dbId: 22, email: "h@h.com", userId: 2211,
-role: "admin",
-githubToken: "github",
-startTrail: () => {
-    return "trail started"
-},
-getCoupon: (name: "hitesh10", off: 10) => {
-    return 10
-}
-}
-hitesh.email = "h@hc.com"
+const hitesh: Admin = {
+  dbId: 22,
+  email: "h@h.com",
+  userId: 2211,
+  role: "admin",
+  githubToken: "github",
+  startTrail: () => {
+    return "trail started";
+  },
+  getCoupon: (name: "hitesh10", off: 10) => {
+    return 10;
+  },
+};
+hitesh.email = "h@hc.com";
 // hitesh.dbId = 33
---
+++ Typescript/pure-typescript/dist/detection.js
@@ -0,0 +1,71 @@
+"use strict";
+function detectType(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("Please provide ID");
+        return;
+    }
+    id.toLowerCase();
+}
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+}
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird Food";
+    }
+}
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    //return shape.side * shape.side
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
+}
