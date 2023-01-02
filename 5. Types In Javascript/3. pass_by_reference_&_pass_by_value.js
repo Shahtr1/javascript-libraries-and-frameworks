09f4a9d5acd2e538eae5d689962a0bc206f1adcc
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-01-02 19:42:00 +0530

    clone deeply

--- 5. Types In Javascript/3. pass_by_reference_&_pass_by_value.js
+++ 5. Types In Javascript/3. pass_by_reference_&_pass_by_value.js
@@ -23,3 +23,30 @@ obj2.password = "easypeasy";
 
 console.log(obj1); // { name: "Yao", password: "easypeasy" }
 console.log(obj2); // { name: "Yao", password: "easypeasy" }
+
+// How to avoid it in arrays?
+
+var c = [1, 2, 3, 4, 5];
+var d = [].concat(c);
+var e = [...c];
+d.push(12344);
+
+// How to avoid it in objects?
+
+let obj = { a: "a", b: "b", c: "c" };
+let clone = Object.assign({}, obj);
+
+obj.c = 5;
+
+let clone2 = { ...obj };
+
+// what if obj has another object in its key value?
+
+let obj = { a: "a", b: "b", c: { deep: "try and copy me" } };
+
+let clone3 = { ...obj }; // this is called shallow clone
+
+obj.c.deep = "hahahaha"; // it gets overwritten
+
+// how to solve it?
+let superClone = JSON.parse(JSON.stringify(obj)); // it will clone deeply
